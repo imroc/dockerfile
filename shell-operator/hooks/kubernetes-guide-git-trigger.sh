75d1f17e3f0fe60f7f6a1b7af3b8630fad3976c7
roc <roc@imroc.cc>
2022-10-03 11:22:37 +0800

    update

---
+++ shell-operator/hooks/kubernetes-guide-git-trigger.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "--config" ]] ; then
+  cat <<EOF
+{
+  "configVersion":"v1",
+  "schedule": [
+    {
+      "name": "every 5 sec",
+      "crontab": "*/5 * * * * *"
+    }
+  ]
+}
+EOF
+else
+  if [ ! -d "/kubernetes-guide" ]; then
+    cd / && git clone --depth=1 https://github.com/imroc/kubernetes-guide.git
+  fi
+  cd /kubernetes-guide
+  isLatest=`git pull 2>&1 | grep 'Already up to date'`
+  if [ "$isLatest" ]; then
+    echo "no new commit"
+    exit 0
+  fi
+  echo "new commit detected, start task to rebuild book"
+  tkn task start mdbook-build-push -n -p tekton-pipelines srcRepo="https://github.com/imroc/kubernetes-guide.git" -p destRepo="https://gitee.com/imroc/kubernetes-guide-book.git"
+fi
