6d2eb54735068773df5830880d4a7970e25f825c
roc <roc@imroc.cc>
2022-10-03 13:35:14 +0800

    update

--- shell-operator/hooks/kubernetes-guide-git-trigger.sh
+++ shell-operator/hooks/kubernetes-guide-git-trigger.sh
@@ -24,5 +24,5 @@ else
     exit 0
   fi
   echo "new commit detected, start task to rebuild book"
-  tkn task start mdbook-build-push -n tekton-pipelines -p srcRepo="https://github.com/imroc/kubernetes-guide.git" -p destRepo="https://gitee.com/imroc/kubernetes-guide-book.git" -s shell-operator --use-param-defaults
+  tkn task start mdbook-build-push -n tekton-pipelines -p srcRepo="https://github.com/imroc/kubernetes-guide.git" -p destRepo="https://gitee.com/imroc/kubernetes-guide-book.git" -s gitee-imroc --use-param-defaults
 fi
