03cf6f5b2d26b4abb4c32cdec8898f4600910e97
roc <roc@imroc.cc>
2022-10-04 09:24:29 +0800

    update

--- shell-operator/hooks/common/git-mdbook-trigger.sh
+++ shell-operator/hooks/common/git-mdbook-trigger.sh
@@ -12,16 +12,17 @@ if [[ $1 == "--config" ]] ; then
   ]
 }
 EOF
-else
-  if [ ! -d "/$2" ]; then
-    cd / && git clone --depth=1 "https://github.com/imroc/$2.git"
-  fi
-  cd "/$2"
-  isLatest=`git pull 2>&1 | grep 'Already up to date'`
-  if [ "$isLatest" ]; then
-    echo "no new commit"
-    exit 0
-  fi
-  echo "new commit detected, start task to rebuild book"
-  tkn task start mdbook-build-push -n tekton-pipelines -p srcRepo="https://github.com/imroc/$2.git" -p destRepo="https://gitee.com/imroc/$2-book.git" -s gitee-imroc --use-param-defaults
+exit 0
 fi
+
+if [ ! -d "/$2" ]; then
+  cd / && git clone --depth=1 "https://github.com/imroc/$2.git"
+fi
+cd "/$2"
+isLatest=`git pull 2>&1 | grep 'Already up to date'`
+if [ "$isLatest" ]; then
+  echo "no new commit"
+  exit 0
+fi
+echo "new commit detected, start task to rebuild book"
+tkn task start mdbook-build-push -n tekton-pipelines -p srcRepo="https://github.com/imroc/$2.git" -p destRepo="https://gitee.com/imroc/$2-book.git" -s gitee-imroc --use-param-defaults
