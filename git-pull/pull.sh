a279a3f901bca7cf23d46fd0a913372dbe6adf37
roc <roc@imroc.cc>
2023-07-12 19:55:30 +0800

    update at Wed, 12 Jul 2023 19:55:30 +0800

---
+++ git-pull/pull.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -u
+
+dir=${DIR}
+interval=${PULL_INTERVAL-10s}
+
+cd $dir
+
+while true
+do
+    sleep $interval
+    echo "$(date -R)"
+    git fetch
+    HEADHASH=$(git rev-parse HEAD)
+    UPSTREAMHASH=$(git rev-parse @{upstream})
+    if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
+      echo "pulling..."
+      git pull
+      if ["$HOOK_COMMAND" != ""]; then
+        echo "exec hook command"
+        bash -c "$HOOK_COMMAND"
+      fi
+    fi
+done
