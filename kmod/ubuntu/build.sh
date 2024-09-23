190a5eedeab6b41222fc6dafe42ff05788ce5763
roc <roc@imroc.cc>
2024-09-23 19:06:19 +0800

    update at 2024-09-23 19:06:19

---
+++ kmod/ubuntu/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+UBUNTU_VERSION=$1
+
+if [ -z "${UBUNTU_VERSION}" ]; then
+  echo "need ubuntu version"
+  exit 1
+fi
+
+IMAGE_NAME="docker.io/imroc/kmod:ubuntu:${UBUNTU_VERSION}"
+
+extra_arg=""
+
+if ! [ -z "${UBUNTU_VERSION}" ]; then
+  extra_arg="--build-arg UBUNTU_VERSION=${UBUNTU_VERSION}"
+fi
+
+docker buildx build $extra_arg -t ${IMAGE_NAME} --push .
